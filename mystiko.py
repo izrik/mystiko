76d668f075d2d912fe0c45757b66856ab4932233
izrik <izrik>
2017-05-01 12:20:15 -0500

    Add a db connection, and a command to create the db tables.

--- mystiko.py
+++ mystiko.py
@@ -24,6 +24,7 @@ from os import environ
 
 import git
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 __version_tuple__ = (0, 1)
 __version__ = '.'.join(str(i) for i in __version_tuple__)
@@ -38,6 +39,7 @@ except git.InvalidGitRepositoryError:
 class Config(object):
     DEBUG = environ.get('MYSTIKO_DEBUG', False)
     PORT = environ.get('MYSTIKO_PORT', 5352)
+    DB_URI = environ.get('MYSTIKO_DB_URI', 'sqlite:////tmp/blog.db')
 
 
 if __name__ == "__main__":
@@ -46,12 +48,22 @@ if __name__ == "__main__":
                         default=Config.DEBUG)
     parser.add_argument('--port', type=int, default=Config.PORT,
                         help='')
+    parser.add_argument('--db-uri', type=str, action='store',
+                        default=Config.DB_URI)
+
+    parser.add_argument('--create-db', action='store_true')
 
     args = parser.parse_args()
 
     Config.DEBUG = args.debug
+    Config.PORT = args.port
+    Config.DB_URI = args.db_uri
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.DB_URI
+
+db = SQLAlchemy(app)
+app.db = db
 
 
 @app.route("/")
@@ -59,14 +71,24 @@ def index():
     return '', 200
 
 
+def create_db():
+    db.create_all()
+
+
 def run():
     print('Mystiko {}'.format(__version__))
     print('  Revision {}'.format(__revision__))
     print('  Debug: {}'.format(Config.DEBUG))
     print('  Port: {}'.format(Config.PORT))
+    if Config.DEBUG:
+        print('  DB URI: {}'.format(Config.DB_URI))
 
-    app.run(debug=Config.DEBUG, port=Config.PORT,
-            use_reloader=Config.DEBUG)
+    if args.create_db:
+        print('Setting up the database')
+        create_db()
+    else:
+        app.run(debug=Config.DEBUG, port=Config.PORT,
+                use_reloader=Config.DEBUG)
 
 
 if __name__ == "__main__":
